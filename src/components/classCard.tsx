0cccfd4cbb1e7631a5473858a404ea25d979c2de
Austin Totty <awtotty@gmail.com>
2024-08-22 22:15:43 -0700

    missed one

--- src/components/classCard.tsx
+++ src/components/classCard.tsx
@@ -49,7 +49,7 @@ export const ClassCard = ({
                 ? `$${enrollment.checkingAccountBalance}`
                 : "-"}
             </div>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <button className="bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded">
               Send
             </button>
           </div>
