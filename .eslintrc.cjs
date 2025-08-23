38bad76604969ed7ff34c0a09c43d2f9db476ac1
Austin Totty <awtotty@gmail.com>
2025-08-23 10:53:31 -0700

    Omit standalone util from type checks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,9 @@ const config = {
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
   ],
+  ignorePatterns: [
+    "src/utils/clerkUtils/**/*.cjs",
+  ],
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
     // Feel free to reconfigure them to your own preference.
